a4a21540653743e980395231708044c503ce798d
sharif ahmad <sharifahmad2061@gmail.com>
2020-10-09 12:46:35 +0500

    working version with docker compose

--- docker/bag/init.sh
+++ docker/bag/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Start our daemons
-gunicorn --bind unix:/tmp/gunicorn_flask.sock -w 4 -D --pythonpath /usr/src/app app:app
+# gunicorn --bind unix:/tmp/gunicorn_flask.sock -w 4 --pythonpath /usr/src/app app:app
 /usr/sbin/nginx
 
 # Gunicorn/Flask running on port 5000, instead of a socket
-#gunicorn -w 4 -b :5000 app:app -D
+gunicorn -w 4 -b :5000 app:app
 
 # Start the bash shell, so the image stays alive and we can access it later
 /bin/bash
